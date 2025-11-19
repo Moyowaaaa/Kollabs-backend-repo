ff849e7e5278a314fabd2d36a3722a013497818c
Moyowaaaa <moyowa.etchie@voyatekgroup.com>
2025-11-19 21:17:39 +0100

    🚀

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,7 +30,7 @@ export default tseslint.config(
           argsIgnorePattern: "^_",
         },
       ],
-      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
   }
 );
